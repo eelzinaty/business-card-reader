cef97f98b8f3f2469ee71fc69d848ab4c082ca5e
Issam Zinati <engessam1985@gmail.com>
2019-08-02 22:27:20 +0300

    Upload Image + Read text using Textract

--- src/app.js
+++ src/app.js
@@ -8,12 +8,38 @@ Amplify.configure(awsconfig);
 Amplify.addPluggable(new AmazonAIPredictionsProvider());
 
 async function createNewTodo() {
-  console.log("In")
+    console.log("In")
 }
 
-const MutationButton = document.getElementById('MutationEventButton');
+//const MutationButton = document.getElementById('MutationEventButton');
 const MutationResult = document.getElementById('MutationResult');
+const UploadImageForm = document.getElementById('UploadImageForm');
 
-MutationButton.addEventListener('click', (evt) => {
-  MutationResult.innerHTML = `MUTATION RESULTS:`;
+UploadImageForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    let photo = document.getElementById("fileToUpload").files[0];
+    let formData = new FormData();
+
+    formData.append("photo", photo);
+    Storage.put('test.jpg', photo,
+        {
+            level: 'public'
+        }
+    ).then(data => {
+        console.log(data);
+        Predictions.identify({
+            text: {
+                source: {
+                    key: data.key,
+                    level: 'public'
+                }
+            }
+        }).then((response) => {
+            MutationResult.innerHTML = `MUTATION RESULTS:` + JSON.stringify(response);
+        });
+    })
+    .catch(error => {
+        console.log(error);
+    });
+    
 });
