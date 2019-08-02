a85b7db316b0f63b2efa5a76742cc944959e8541
Issam Zinati <engessam1985@gmail.com>
2019-08-02 21:17:37 +0300

    Amplify Init

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import Amplify from '@aws-amplify/core';
+import Predictions, { AmazonAIPredictionsProvider } from '@aws-amplify/predictions';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
+Amplify.addPluggable(new AmazonAIPredictionsProvider());
+
+async function createNewTodo() {
+  console.log("In")
+}
+
+const MutationButton = document.getElementById('MutationEventButton');
+const MutationResult = document.getElementById('MutationResult');
+
+MutationButton.addEventListener('click', (evt) => {
+  MutationResult.innerHTML = `MUTATION RESULTS:`;
+});
